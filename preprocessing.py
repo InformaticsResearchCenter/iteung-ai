88d66a2807d6f7d04482e8d2d7d9842f18524636
Rolly Maulana Awangga <rolly@awang.ga>
2022-09-16 16:31:09 +0700

    add str

--- preprocessing.py
+++ preprocessing.py
@@ -101,14 +101,14 @@ data_length = 0
 filename= './dataset/clean_qa.tx'
 with open(filename, 'w', encoding='utf-8') as f:
   for index, row in df.iterrows():
-    question = normalize_sentence(row['question'])
+    question = normalize_sentence(str(row['question']))
     question = normalize_sentence(question)
     question = stemmer.stem(question)
 
-    answer = row['answer'].lower().replace('iteung', 'aku').replace('\n', ' ')
+    answer = str(row['answer']).lower().replace('iteung', 'aku').replace('\n', ' ')
 
     if len(question.split()) > 0 and len(question.split()) < 13 and len(answer.split()) < 29:
-      body="{"+str(question)+"}|<START> {"+str(answer)+"} <END>\n"
+      body="{"+question+"}|<START> {"+answer+"} <END>\n"
       print(body, file=f)
       #filename.write(f"{question}\t<START> {answer} <END>\n")
 
